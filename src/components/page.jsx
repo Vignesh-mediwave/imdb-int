e0cfaf6b2a93d8efff9f662982e6580b23f092ad
Vignesh-mediwave <vignesh@mindwaveventures.com>
2022-03-09 19:09:05 +0530

    title added by using useEffect

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 const Page = (props) => {
+  useEffect(() => {
+    if (props.title) {
+      document.title = props.title;
+    }
+  }, [props.title]);
   return <div className="container">{props.children}</div>;
 };
 
